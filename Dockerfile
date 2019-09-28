fe87ba983b2e6fb4e1638401db6064aed85fa846
เบด <wuttipongt@Wuttipongs-MacBook-Pro.local>
2019-09-28 14:17:08 +0700

    initialize

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM centos:7
+
+COPY installer/"*.zip" /installer/
+COPY installer/oraInst.loc /etc/
+
+ENV ORACLE_BASE=/u01/app/oracle \
+    ORACLE_HOME=/u01/app/oracle/product/11.2.0/dbhome_1 \
+    PATH=${PATH}:/u01/app/oracle/product/11.2.0/dbhome_1/bin \
+    _JAVA_OPTIONS=-Xmx1024M
+
+COPY installer/db_install.rsp installer/netca.rsp installer/dbca.rsp ${ORACLE_BASE}/
+
+RUN yum -y install wget && \
+        wget https://yum.oracle.com/RPM-GPG-KEY-oracle-ol7 -O /etc/pki/rpm-gpg/RPM-GPG-KEY-oracle && \
+        gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-oracle && \
+        curl http://public-yum.oracle.com/public-yum-ol7.repo -#o /etc/yum.repos.d/public-yum-ol7.repo && \
+        rpm -e --nodeps $(rpm -qa | grep "centos-release") && \
+        yum -y install oracle-rdbms-server-11gR2-preinstall perl unzip && \
+        mkdir -p ${ORACLE_BASE} && chown oracle:oinstall ${ORACLE_BASE} && \
+        chown -R oracle:oinstall ${ORACLE_BASE}/db_install.rsp ${ORACLE_BASE}/netca.rsp ${ORACLE_BASE}/dbca.rsp /installer
+
+WORKDIR /installer
+
+USER oracle
+RUN unzip "*.zip" && \
+    /installer/database/runInstaller -ignoresysprereqs -ignoreprereq -waitforcompletion -force -silent ORACLE_HOME=${ORACLE_HOME} ORACLE_HOME_NAME=orcl -responseFile ${ORACLE_BASE}/db_install.rsp DECLINE_SECURITY_UPDATES=true ORACLE_BASE=${ORACLE_BASE} && \
+    rm -rf /installer/* && \
+    echo "# fix $ORACLE_HOME/bin/oracle command empty file" && \
+    if [ ! -s ${ORACLE_HOME}/bin/oracle ]; then make -f ${ORACLE_HOME}/rdbms/lib/ins_rdbms.mk javavm_setup_default_jdk; ${ORACLE_HOME}/bin/relink as_installed; fi
+
+USER root
+RUN $ORACLE_HOME/root.sh && \
+    yum -y remove wget unzip
+
+COPY installer/entrypoint.sh /
+RUN chown -R oracle:oinstall /entrypoint.sh && chmod u+x /entrypoint.sh
+
+USER oracle
+
+WORKDIR ${ORACLE_HOME}
+ENTRYPOINT ["/entrypoint.sh"]
+
+EXPOSE 1521 5500 5520
+
+ENV MEMORY_PERCENTAGE=30 EM_CONFIGURATION=LOCAL ORACLE_SGA_TARGET=512m ORACLE_PGA_TARGET=512m DISPLAY=hostname:0.0
---
+++ installer/entrypoint.sh
@@ -0,0 +1,109 @@
+#!/bin/bash -eu
+
+set +u
+[ "${DEBUG}" ] && set -x
+set -u
+
+ORA_DATA=${ORACLE_BASE}/oradata
+
+function verify_environment() {
+  set +eu
+  local error=false
+
+  INITIALIZED=false
+
+  INIT_FILE=$(ls ${ORA_DATA}/init*.ora 2>/dev/null)
+  
+  if [ -z "$INIT_FILE"]; then INITIALIZED=false; else INITIALIZED=true && ORACLE_SID=$(basename ${INIT_FILE} | cut -f 1 -d"." | cut -b5-); fi
+
+  if [ -z ${ORACLE_SID} ]; then echo "[ERROR] You need to specify the desired ORACLE_SID" && error=true; fi
+
+     if ! $INITIALIZED; then
+ 	#if [ -z ${ORACLE_DATABASE} ]; then echo "[ERROR] You need to specify the desired ORACLE_DATABASE (8 chars max)" && error=true; fi
+ 		
+ 	if [ -z ${ORACLE_USER} ]; then echo "[ERROR] You need to specify the desired ORACLE_USER" && error=true; fi
+ 		
+ 	if [ -z ${ORACLE_PASSWORD} ]; then echo "[ERROR] You need to specify the desired ORACLE_PASSWORD" && error=true; fi
+ 		
+ 	if [ -z ${ORACLE_DBA_PASSWORD} ]; then echo "[ERROR] You need to specify the desired ORACLE_DBA_PASSWORD" && error=true; fi
+     fi
+
+     if ${error}; then exit 1; fi
+
+     echo "[INFO] Oracle SGA and PGA : ${MEMORY_PERCENTAGE}"
+
+     set -eu
+}
+
+function stop_database() {
+  ${ORACLE_HOME}/bin/sqlplus / as sysdba <<EOF
+  shutdown immediate;
+  exit;
+EOF
+
+}
+
+function start_database() {
+  ${ORACLE_HOME}/bin/sqlplus / as sysdba <<EOF
+  startup;
+  exit;
+EOF
+
+}
+
+function start_listener() {
+  #export DISPLAY=hostname:0.0
+  #$ORACLE_HOME/bin/netca -ignoresysprereqs -ignoreprereq -waitforcompletion -force -silent -responseFile ${ORACLE_BASE}/netca.rsp
+
+  printf "LISTENER=(DESCRIPTION_LIST=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=0.0.0.0)(PORT=1521))(ADDRESS=(PROTOCOL=IPC)(KEY=EXTPROC1521))))\n" > $ORACLE_HOME/network/admin/listener.ora
+  $ORACLE_HOME/bin/lsnrctl start
+}
+
+function ceate_user() {
+  echo "create user ${ORACLE_USER} identified by ${ORACLE_PASSWORD};" | ${ORACLE_HOME}/bin/sqlplus / as sysdba
+  echo "GRANT ALL PRIVILEGES to ${ORACLE_USER};" | ${ORACLE_HOME}/bin/sqlplus / as sysdba
+
+  echo "[INFO] user ${ORACLE_USER} created"
+}
+
+function update_variables() {
+  ${ORACLE_HOME}/bin/sqlplus / as sysdba <<EOF
+	alter system set sga_target=${ORACLE_SGA_TARGET} scope=spfile;
+	alter system set sga_max_size=${ORACLE_SGA_TARGET} scope=spfile;
+	alter system set pga_aggregate_target=${ORACLE_PGA_TARGET} scope=spfile;
+	alter system set FILESYSTEMIO_OPTIONS=asynch scope=spfile;
+  exit;
+EOF
+}
+
+function intialize () {
+  verify_environment
+
+  start_listener
+
+  if $INITIALIZED; then
+    echo "[INFO] Database already initialized, just starting it"
+    cp -v ${ORA_DATA}/init${ORACLE_SID}.ora ${ORACLE_HOME}/dbs 
+    cp -v ${ORA_DATA}/tnsnames.ora ${ORACLE_HOME}/network/admin
+
+    start_database
+    return
+  fi
+
+   $ORACLE_HOME/bin/dbca -silent -createDatabase -responseFile ${ORACLE_BASE}/dbca.rsp -gdbname ${ORACLE_SID} -sid ${ORACLE_SID} -syspassword ${ORACLE_DBA_PASSWORD} -systempassword ${ORACLE_DBA_PASSWORD} -dbsnmppassword ${ORACLE_DBA_PASSWORD} -memoryPercentage ${MEMORY_PERCENTAGE} -emConfiguration ${EM_CONFIGURATION}
+
+   ceate_user
+
+   #update_variables
+
+   #we need to restart after the properties update
+   stop_database
+   start_database
+
+   cp ${ORACLE_HOME}/dbs/init.ora ${ORA_DATA}/init${ORACLE_SID}.ora
+   cp ${ORACLE_HOME}/network/admin/tnsnames.ora ${ORA_DATA}
+}
+
+intialize
+
+tail -F /dev/null
